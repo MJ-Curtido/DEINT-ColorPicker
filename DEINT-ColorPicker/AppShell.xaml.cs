02d22ca93064ef48e500dfdfaf89648564c99691
MJ-Curtido <manu17pc@gmail.com>
2022-12-12 11:14:00 +0100

    Agregar archivos de proyecto.

---
+++ DEINT-ColorPicker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DEINT_ColorPicker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DEINT-ColorPicker/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace DEINT_ColorPicker;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+	{
+		InitializeComponent();
+    }
+
+	private void SliderColor_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		var col = Color.FromRgb((float)sliderRed.Value, (float)sliderGreen.Value, (float)sliderBlue.Value);
+
+		conteiner.BackgroundColor = col;
+		colorActual.Text = "#" + col.ToHex();
+	}
+
+	private void colorActual_Clicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
