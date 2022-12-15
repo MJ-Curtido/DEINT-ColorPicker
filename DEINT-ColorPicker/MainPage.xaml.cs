76bc1c285a80de36e7e901accca153aee2cedd02
MJ-Curtido <manu17pc@gmail.com>
2022-12-15 14:28:02 +0100

    Fin

--- DEINT-ColorPicker/MainPage.xaml.cs
+++ DEINT-ColorPicker/MainPage.xaml.cs
@@ -15,12 +15,24 @@ public partial class MainPage : ContentPage
 		var col = Color.FromRgb((float)sliderRed.Value, (float)sliderGreen.Value, (float)sliderBlue.Value);
 
 		conteiner.BackgroundColor = col;
-		colorActual.Text = "#" + col.ToHex();
+		colorActual.Text =  col.ToHex();
 	}
 
 	private void colorActual_Clicked(object sender, EventArgs e)
 	{
-
+		Clipboard.Default.SetTextAsync(colorActual.Text);
 	}
+
+	private void colorRandom_Clicked(object sender, EventArgs e)
+    {
+        Random random = new Random();
+		sliderRed.Value = random.NextDouble();
+		sliderGreen.Value = random.NextDouble();
+		sliderBlue.Value = random.NextDouble();
+        var col = Color.FromRgb((float)sliderRed.Value, (float)sliderGreen.Value, (float)sliderBlue.Value);
+
+        conteiner.BackgroundColor = col;
+        colorActual.Text = col.ToHex();
+    }
 }
 
